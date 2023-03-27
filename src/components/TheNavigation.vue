0be63939eec54e2fb367aded40ac537ae03b4a73
LorisBasile <loris.basile@contactodigital.it>
2023-03-27 17:02:34 +0200

    eliminazione delle view

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,16 +1,26 @@
     <template>
         <nav id="nav">
-            <router-link to="/">Prova</router-link> 
-            <router-link to="/brazil">Brazil</router-link>
-            <router-link to="/jamaica">Jamaica</router-link>
-            <router-link to="/panama">Panama</router-link>
-            <router-link to="/hawaii">Hawaii</router-link>
+            <router-link to="/" id="logo">Travel App</router-link> 
+             <router-link v-for="destination in destinations" :key="destination.id" :to="{ name: 'destination.show', params: {id:destination.id, slug:destination.slug}}"  >
+                        {{destination.name}}
+            </router-link> 
         </nav>
     </template>
 
     <style lang="css">
         #nav .vue-forte{
-            color: rgb(0, 229, 255);
-            border-bottom: 2px solid rgb(89, 0, 255) ;
+            color: rgb(0, 255, 234);
+            border-bottom: 2px solid rgb(0, 255, 234) ;
         }
     </style>
+
+    <script>
+        import sourceData from '@/data.json'
+        export default {
+            data(){
+                return {
+                    destinations: sourceData.destinations,
+                };
+            },
+        };
+    </script>
--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,7 @@ import HomeView from "../views/HomeView.vue";
 
 const routes = [
   { path: "/", name: "home", component: HomeView },
-
-  { path: "/brazil", name: "brazil",   component: () => import( /* webpackChunkName: "brazil" */'@/views/Brazil.vue') },
-  { path: "/jamaica", name: "jamaica", component: () => import( /* webpackChunkName: "jamaica" */'@/views/Jamaica.vue') },
-  { path: "/panama", name: "panama",   component: () => import( /* webpackChunkName: "panama" */'@/views/Panama.vue') },
-  { path: "/hawaii", name: "hawaii",   component: () => import( /* webpackChunkName: "hawaii" */'@/views/Hawaii.vue') },
-  { path: "/destination/:id", component: () => import('@/views/DestinationShow.vue') },
+  { path: "/Destination/:id/:slug", name:"destination.show", component: () => import('@/views/DestinationShow.vue') },
 ];
 
 const router = createRouter({
